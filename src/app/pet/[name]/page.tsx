3ae4e78fb947bfa41bf6863cf0a72d74fe63fb01
hudsonborgesbrito <Teste123@987>
2024-09-30 19:32:58 +0100

    fix lint issues

--- src/app/pet/[name]/page.tsx
+++ src/app/pet/[name]/page.tsx
@@ -1,14 +1,15 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
-async function getPet(name){
+async function getPet(name: string){
     const allPetsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
     const allPets = await allPetsPromise.json()
-    const pet = allPets.filter(pet => {
+    const pet = allPets.filter((pet: any) => {
         return name.toUpperCase() === pet.name.toUpperCase()
     })[0]
     return pet
 }
 
-export default async function Pet({params}){
+export default async function Pet({params}: any){
     const pet = await getPet(params.name)
     return (
         <div>
